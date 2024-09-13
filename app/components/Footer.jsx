3a3507cf70b1309d376cdeeeb1c0a9741842fe2f
shreyyyyy <jain.shreyans0601@gmail.com>
2024-09-13 19:52:28 +0530

    Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="footer border border-black z-10 bg-gradient-to-br from-gray-900 via-transparent to-gray-900 bg-opacity-50">
       <div className="container p-12 flex justify-between">
         <p className="text-slate-600">Created By: Shreyans Jain</p>
-        <p className="text-slate-600">All rights reserved. @2024</p>
+        <p className="text-slate-600">All rights reserved. © 2024</p>
       </div>
     </footer>
 
