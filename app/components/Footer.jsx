d075dc380189aaffe594a6222d0eaaab887fe815
shreyyyyy <jain.shreyans0601@gmail.com>
2024-09-13 18:39:49 +0530

    Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer border z-10 bg-gradient-to-br from-black to-gray-900 border-l-transparent border-r-transparent text-blue-900">
+    <footer className="footer border border-black z-10 bg-gradient-to-br from-gray-900 via-transparent to-gray-900 bg-opacity-50">
       <div className="container p-12 flex justify-between">
         <p className="text-slate-600">Created By: Shreyans Jain</p>
         <p className="text-slate-600">All rights reserved. @2024</p>
       </div>
     </footer>
+
   );
 };
 
